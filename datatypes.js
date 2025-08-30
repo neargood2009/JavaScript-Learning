f87838a974d8c5bf7e45facfed120c6488a106ec
Pascoala Dsouza <neargood2009@gmail.com>
2025-08-30 20:15:07 +0530

    datatypes: lecture 1 notes

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+// number
+let age = 24;
+console.log(typeof age);
+let price = 100.5;
+console.log(typeof price);
+
+// string
+let fullName = "tony stark";
+console.log(typeof fullName);
+
+// boolean
+let isFollow = true;
+console.log(typeof isFollow);
+
+//undefined
+let x;
+console.log(typeof x);
+
+//null
+let y=null;
+console.log(typeof y) // object (null means absence of object)
+
+//bigint
+let z = BigInt("123");
+console.log(z);
+console.log(typeof z)
+
+//symbol
+let s = Symbol("Hello!");
+console.log(s);
+console.log(typeof s);
